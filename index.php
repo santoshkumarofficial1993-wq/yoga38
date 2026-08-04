8c3f48672d554555220a23f583ad84c8f45571bc
santoshkumarofficial1993-wq <santoshkumarofficial1993@gmail.com>
2026-08-04 16:41:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -108,11 +108,12 @@ function truncateExcerpt($text, $limit = 100) {
    
 
 <!-- Privacy-friendly analytics by Plausible -->
-<script async src="https://plausible.io/js/pa-OQLBI7mPKDqtbvKTPpi-W.js"></script>
+<script async src="https://plausible.io/js/pa-vG520MffpIp3CmDary9cN.js"></script>
 <script>
   window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
   plausible.init()
 </script>
+
 
 
    
